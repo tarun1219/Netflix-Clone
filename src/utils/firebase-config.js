8360e7611d87c0080a438f3ddaea22f611455b98
tarun1219 <41871545+tarun1219@users.noreply.github.com>
2023-03-02 03:35:07 -0800

    Add files via upload

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAS-Z9wWZA0Hae-8CemfzSBnhzfYDIMP0g",
@@ -7,10 +7,9 @@ const firebaseConfig = {
   projectId: "react-netflix-clone-5d043",
   storageBucket: "react-netflix-clone-5d043.appspot.com",
   messagingSenderId: "314221416751",
-  appId: "1:314221416751:web:7b80a94d89c664aeff91da",
-  measurementId: "G-F4TYERWSG4"
+  appId: "1:314221416751:web:555328ae8492fc30ff91da",
+  measurementId: "G-068H6QCJM1"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
+
